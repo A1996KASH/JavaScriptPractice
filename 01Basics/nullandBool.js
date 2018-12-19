9444361aa0856579b10762a5c8270fe71cc27531
Akash Sengar <akashsengar96@gmail.com>
2018-12-19 13:22:59 +0530

    Basics Done

---
+++ 01Basics/nullandBool.js
@@ -0,0 +1,23 @@
+// assume temp is coming from website API
+
+var temp ;
+
+// done some calculation 
+
+//temp = 2;
+console.log("Current Temperature is: " + temp);
+
+// grade System
+
+// 10 - Great Job
+// 5 - Work Harder
+// 2 - Poor
+// 0 -fail
+
+//Boolean Operations - true/false
+let actualMarks = 10;
+
+let myGrade = (actualMarks == 10); 
+//let grade =  true;
+
+console.log(myGrade);

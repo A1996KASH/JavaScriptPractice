4acf011395e1d1958f7253da315dff79d68275db
Akash Sengar <akashsengar96@gmail.com>
2018-12-19 12:15:13 +0530

    first Commit

---
+++ 01Basics/variables.js
@@ -0,0 +1,23 @@
+// space in Memory is a variable 
+let name = 'Akash';
+
+
+let score = '100'; //not a number its  a string as written in Quotes
+
+let bonus = '20';
+
+let totalScore = score + bonus;
+
+let newScore = 100; // type is number 
+
+let newBonus = 20;
+
+console.log(newScore);
+console.log(score);
+console.log(totalScore); // 10020 concat because of type string.
+console.log(newScore + newBonus); // 120 
+
+let firstName = 'Akash';
+let lastName = 'Sengar';
+
+console.log(firstName + ' ' + lastName);

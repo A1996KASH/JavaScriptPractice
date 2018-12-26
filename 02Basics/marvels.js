932afa8c928afac643f264bdf0b4f2bcef857f1e
Akash Sengar <akashsengar96@gmail.com>
2018-12-26 13:08:58 +0530

    Array Methods

--- 02Basics/marvels.js
+++ 02Basics/marvels.js
@@ -1,7 +1,31 @@
-// datatypes - Array
+// // datatypes - Array
 
-const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America'];
-console.log(superHeroes);
-console.log(superHeroes[0]);
-console.log(superHeroes[superHeroes.length - 1 ]);
-console.log(`we have ${superHeroes.length} super Heroes`)
+// const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America'];
+// console.log(superHeroes);
+// console.log(superHeroes[0]);
+// console.log(superHeroes[superHeroes.length - 1 ]);
+// console.log(`we have ${superHeroes.length} super Heroes`)
+
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+
+// numbers[1] = 'SomeThing';
+
+// console.log(numbers);
+// start
+
+// numbers.shift(); // removes first element 
+// console.log(numbers);
+// numbers.unshift('something');
+// console.log(numbers);
+
+// End
+
+// numbers.pop(); // last element to be deleted
+// numbers.push('Seven') // add element addd the end
+
+// console.log(numbers)
+
+// Middle
+
+numbers.splice(2,2, 'Something');
+console.log(numbers);

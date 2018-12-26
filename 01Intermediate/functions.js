2457423d5107a9ef2fb5054699544833e57dfcf4
Akash Sengar <akashsengar96@gmail.com>
2018-12-26 12:59:47 +0530

    Function

---
+++ 01Intermediate/functions.js
@@ -0,0 +1,16 @@
+
+let sayHello = function (name) {
+    console.log('Hey '  + name);
+}
+
+sayHello('Jhon');
+sayHello('Akash');
+
+let fullNameMaker = function (firstname, lastname) {
+
+    console.log(`Welcome ${firstname} ${lastname}`);
+    console.log('Happy to have you onboard');
+
+}
+
+fullNameMaker('Akash','Sengar');
---
+++ 02Basics/marvels.js
@@ -0,0 +1,7 @@
+// datatypes - Array
+
+const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America'];
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[superHeroes.length - 1 ]);
+console.log(`we have ${superHeroes.length} super Heroes`)
